8ee04c0917d48a87f530282f003bd7a6b7361272
bcactussie <bastiaanlovers@icloud.com>
2022-08-02 09:45:48 -0400

    Add the function playGround. This function tells who won the game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log ("Hello World");
+console.log ("Rock, Paper, Scissors Game");
 
 function getComputerChoice() { 
     let randomNumb = Math.random() * 3; // Random number between 0 - 3
@@ -21,5 +21,53 @@ let playerSelection = prompt("Choose: Rock, Paper, Scissors ");
 return playerSelection.toLowerCase(); // lowercase the return value 
 }
 
+
+let playerChoice = selectPlayer();
+let computerChoice = getComputerChoice();
+
+// Tells you who won the game 
+function playGround (playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "its a tie!";
+    }
+    else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
+        return "Computer wins";
+    }
+    else if ((playerChoice === "rock") && (computerChoice === "paper")) {
+        return "Computer wins";
+    }    
+    else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
+        return "Computer wins";
+    }    
+    else {
+        return "Player wins!";
+    }
+}
+
+console.log ("Player choose:"+ " " + playerChoice);
+console.log ("Computer choose:" + " " + computerChoice);
+
+console.log (playGround(playerChoice, computerChoice));
+
+
+
+
+
+
+// function with 2 parameters that compares the choises of the player and computer in order to see who wins. 
+/* function playGround (selectPlayer, getComputerChoice) { 
+    if (selectPlayer === getComputerChoice) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+console.log (playGround());
+
+
+
 console.log (selectPlayer());
-console.log( getComputerChoice())
+console.log( getComputerChoice()); */ 
