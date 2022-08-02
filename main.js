c7b4b7022f13a112765310968698f5171ea1122f
bcactussie <bastiaanlovers@icloud.com>
2022-08-02 08:08:12 -0400

    Added values rock, scissors and paper

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 console.log ("Hello World");
 
 function getComputerChoice() { 
-    let randomNumb = Math.random() * 3;
+    let randomNumb = Math.random() * 3; // Random number between 0 - 3
+     // return value of the random number
+    
+    if (randomNumb < 1) {
+        return "rock"
+    }
+    else if (randomNumb < 2) {
+        return "scissors"
+    }
+    else if (randomNumb < 3) {
+        return "paper"
+    }
     return randomNumb;
 }
 
-console.log( getComputerChoice());
+console.log( getComputerChoice())
