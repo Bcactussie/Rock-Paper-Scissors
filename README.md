c38a4b946ad72bf8f34f5e8383cacc7e584aaaff
bcactussie <bastiaanlovers@icloud.com>
2022-08-02 08:03:43 -0400

    Generate random number between 0 - 3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rock-Paper-Scissors
 Rock Paper Scissors game. 
+
+1. Started with setting up the computer function. We need a random selected choice of Rock, Paper, Scissors. 
+
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log ("Hello World");
+
+function getComputerChoice() { 
+    let randomNumb = Math.random() * 3;
+    return randomNumb;
+}
+
+console.log( getComputerChoice());
