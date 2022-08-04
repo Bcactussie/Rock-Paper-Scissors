591a65a6d34d01fe1a1386d7e39b70eacca04b77
bcactussie <bastiaanlovers@icloud.com>
2022-08-04 07:38:40 -0400

    Finished the game over the past few days! Have fun

--- main.js
+++ main.js
@@ -1,73 +1,70 @@
 console.log ("Rock, Paper, Scissors Game");
 
-function getComputerChoice() { 
-    let randomNumb = Math.random() * 3; // Random number between 0 - 3
-     // return value of the random number
-    
-    if (randomNumb < 1) {
-        return "rock"
-    }
-    else if (randomNumb < 2) {
-        return "scissors"
-    }
-    else if (randomNumb < 3) {
-        return "paper"
-    }
-    return randomNumb;
+// Computer chooses randomly 
+
+function getComputerChoice () {
+     let randomNumb = Math.random() * 3;
+     if (randomNumb < 1) {
+        return randomNumb = "rock";
+     }
+     else if (randomNumb < 2) {
+        return randomNumb = "paper";
+     }
+     else {
+        return randomNumb = "scissors";
+     }
+
 }
 
 function selectPlayer (){ // Prompt choice for the player
-let playerSelection = prompt("Choose: Rock, Paper, Scissors ");
-return playerSelection.toLowerCase(); // lowercase the return value 
+    let playerSelection = prompt("Choose: Rock, Paper, Scissors ");
+    return playerSelection.toLowerCase();
+    }
+
+ const playerSelection = selectPlayer;
+ const computerSelection = getComputerChoice();  
+ 
+let computerScore = 0;
+ let playerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === computerSelection)) {
+        return "It's a tie";
+    }
+    else if (((playerSelection === "paper") && computerSelection === "scissors"  )) {
+        return "Player chooses: paper, computer chooses scissors! COMPUTER WINS" + computerScore++;
+    }
+    else if (((playerSelection === "rock") && computerSelection === "paper"  )) {
+        return "Player chooses: rock, computer chooses Paper! COMPUTER WINS" + computerScore++;
+    }
+    else if (((playerSelection === "scissors") && computerSelection === "rock"  )) {
+        return  "Player chooses: scissors, computer chooses rock! COMPUTER WINS" + computerScore++;
+    }
+    else if  (((playerSelection === "scissors") && computerSelection === "paper"  )) {
+        return  "Player chooses: scissors, computer chooses paper! PLAYER WINS" + playerScore++; 
+}
+else if  (((playerSelection === "paper") && computerSelection === "rock"  )) {
+    return  "Player chooses: paper, computer chooses rock! PLAYER WINS" + playerScore++; 
+}
+else if  (((playerSelection === "rock") && computerSelection === "scissors"  )) {
+    return  "Player chooses: rock, computer chooses scissors! PLAYER WINS" + playerScore++; 
+}
+}
+
+function score() { for (let i = 0; i < 5; i++) {  
+   console.log (playRound(selectPlayer(), getComputerChoice()));
+}
+if (playerScore > computerScore) {
+    return ("Player wins!" + " " + "player score is:" + " " + playerScore + " " + "computer score:" + " " + computerScore);
+}
+else if (playerScore < computerScore) {
+    return ("Computer wins!" + " " + "player score is:" + " " + playerScore + " " + "computer score:" + " " + computerScore);
+}
+else {
+    return ("It's a tie!");
 }
 
 
-let playerChoice = selectPlayer();
-let computerChoice = getComputerChoice();
-
-// Tells you who won the game 
-function playGround (playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        return "its a tie!";
-    }
-    else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
-        return "Computer wins";
-    }
-    else if ((playerChoice === "rock") && (computerChoice === "paper")) {
-        return "Computer wins";
-    }    
-    else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
-        return "Computer wins";
-    }    
-    else {
-        return "Player wins!";
-    }
 }
 
-console.log ("Player choose:"+ " " + playerChoice);
-console.log ("Computer choose:" + " " + computerChoice);
-
-console.log (playGround(playerChoice, computerChoice));
-
-
-
-
-
-
-// function with 2 parameters that compares the choises of the player and computer in order to see who wins. 
-/* function playGround (selectPlayer, getComputerChoice) { 
-    if (selectPlayer === getComputerChoice) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-console.log (playGround());
-
-
-
-console.log (selectPlayer());
-console.log( getComputerChoice()); */ 
+console.log (score());
